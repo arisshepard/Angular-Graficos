01b712c7ffcd2dcdf29491e017c80b66ba6899a0
Maria Lopez <mlopez@extrasoft.es>
2020-11-04 13:26:21 +0100

    graficas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonutComponent } from './components/donut/donut.component';
+import { RadarComponent } from './components/radar/radar.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LineaComponent, BarraComponent, DonutComponent, RadarComponent],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
